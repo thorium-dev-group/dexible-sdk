bd200be9ab280aba333b9323afc0500108bc1001
Mike Powers <mjpowersjr@gmail.com>
2022-07-18 13:32:48 +0000

    chore: improved jest monorepo support

--- packages/dex-order/jest.config.js
+++ packages/dex-order/jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-const baseConfig = require('../../jest.config');
+const baseConfig = require('../../jest.base');
 
 module.exports = {
   ...baseConfig
